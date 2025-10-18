4ccc6f25854cca5a24d050d2ff71b99876f811e9
clarklindev <clarklindev@gmail.com>
2025-10-18 18:22:45 +0200

    10 - Course Project The Learning Resources App - Setup & First Steps

---
+++ 10 - Course Project The Learning Resources App/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official view documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google',
+          link: 'http://www.google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
